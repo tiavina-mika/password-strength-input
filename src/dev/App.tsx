008b1264f9277440123f1c2174ee3e11905e986a
tiavina-mika <tiavinamika@gmail.com>
2024-06-16 09:43:40 +0300

    Update App.tsx

--- src/dev/App.tsx
+++ src/dev/App.tsx
@@ -1,35 +1,38 @@
-import { Container, ThemeProvider, createTheme } from '@mui/material';
 import PasswordStrengthInput from '../PasswordStrengthInput';
-
-const theme = createTheme();
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <PasswordStrengthInput
-          className='input'
-          options={{
-            tooWeak: {
-              label: 'Too weak 2',
-              color: 'red',
-            },
-            weak: {
-              label: 'Weak 2',
-              color: 'yellow',
-            },
-            medium: {
-              label: 'Medium 2',
-              color: 'green',
-            },
-            strong: {
-              label: 'Strong 2',
-              color: 'blue'
-            },
-          }}
-        />
-      </Container>
-    </ThemeProvider>
+    <div className="app">
+      <div className="app-content">
+        <div>
+          <h1>Password Strength Input</h1>
+          <p>Check the password strength</p>
+        </div>
+        <div>
+          <PasswordStrengthInput
+            className='input'
+            options={{
+              tooWeak: {
+                label: 'Too weak 1',
+                color: 'red',
+              },
+              weak: {
+                label: 'Weak 1',
+                color: 'yellow',
+              },
+              medium: {
+                label: 'Medium 1',
+                color: 'green',
+              },
+              strong: {
+                label: 'Strong 1',
+                color: 'blue'
+              },
+            }}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
